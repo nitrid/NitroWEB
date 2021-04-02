505f408b957a639418dec96d2211b770d2a08379
ArifSezerAkkaya <78169488+ArifSezerAkkaya@users.noreply.github.com>
2021-04-02 18:41:47 +0300

    ...

--- www/js/app.js
+++ www/js/app.js
@@ -42,5 +42,41 @@ angular.module("app",
             templateUrl : "view/mono/MonoMamulMalKabul.html",
             controller:"MonoMamulMalKabul"
         }
+    ).
+    state
+    (
+        {
+            name: 'main.MonoYariMamulBarkodIptal',
+            url: '/mono_yari_mamul_barkod_iptal',
+            templateUrl : "view/mono/MonoYariMamulBarkodIptal.html",
+            controller:"MonoYariMamulBarkodIptal"
+        }
+    ).
+    state
+    (
+        {
+            name: 'main.MonoRafEtiketBasimi',
+            url: '/mono_raf_etiket_basimi',
+            templateUrl : "view/mono/MonoRafEtiketBasimi.html",
+            controller:"MonoRafEtiketBasimi"
+        }
+    ).
+    state
+    (
+        {
+            name: 'main.MonoKasaBarkodOlustur',
+            url: '/mono_kasa_barkod_olustur',
+            templateUrl : "view/mono/MonoKasaBarkodOlustur.html",
+            controller:"MonoKasaBarkodOlustur"
+        }
+    ).
+    state
+    (
+        {
+            name: 'main.MonoSiparisToplamaListesi',
+            url: '/mono_siparis_toplama_listesi',
+            templateUrl : "view/mono/MonoSiparisToplamaListesi.html",
+            controller:"MonoSiparisToplamaListesi"
+        }
     )
 });
---
+++ www/js/ctrl/mono/MonoKasaBarkodOlustur.js
@@ -0,0 +1,4 @@
+function MonoKasaBarkodOlustur($scope,srv)
+{
+
+}
