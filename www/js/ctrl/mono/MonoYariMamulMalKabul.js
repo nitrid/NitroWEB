5a27a91b3afe30e34422bc321c2fe89ce09db91e
mhrgndz <41637496+mhrgndz@users.noreply.github.com>
2021-04-10 17:58:15 +0300

    ..

--- www/js/ctrl/mono/MonoYariMamulMalKabul.js
+++ www/js/ctrl/mono/MonoYariMamulMalKabul.js
@@ -2,38 +2,6 @@ function MonoYariMamulMalKabul($scope,srv)
 {
     $scope.Init = function()
     {
-        $("#GrdList").dxDataGrid
-        (
-            {
-                dataSource: [],
-                allowColumnResizing: true,
-                height: 450,
-                width: "auto",
-                columnWidth: 100,
-                selection: 
-                {
-                    mode: "single"
-                },
-                hoverStateEnabled: true,
-                showBorders: true,
-                paging: 
-                {
-                    pageSize: 10
-                },
-                filterRow: 
-                {
-                    visible: true,
-                    applyFilter: "auto"
-                },
-                headerFilter: 
-                {
-                    visible: true
-                },
-                onSelectionChanged: function (selectedItems) 
-                {
-                    SelectionRow = selectedItems.selectedRowsData[0];
-                }
-            }
-        )
+        
     }
 }
