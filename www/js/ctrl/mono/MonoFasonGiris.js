164f3400ed0bb7cad5bed2183d400414ecd8d233
mhrgndz <41637496+mhrgndz@users.noreply.github.com>
2021-04-16 00:52:19 +0300

    ..

--- www/js/ctrl/mono/MonoFasonGiris.js
+++ www/js/ctrl/mono/MonoFasonGiris.js
@@ -85,24 +85,34 @@ function MonoFasonGiris($scope,srv)
                 db : "{M}." + $scope.Firma,
                 query : "SELECT " +
                         "ISNULL((SELECT TOP 1 bar_kodu FROM BARKOD_TANIMLARI WHERE bar_stokkodu = ISNULL((SELECT TOP 1 upl_kodu FROM URETIM_MALZEME_PLANLAMA WHERE upl_isemri = is_Kod AND upl_uretim_tuket = 1),'') AND bar_birimpntr = 1 AND bar_partikodu = '' AND bar_lotno = 0),'') AS BARKOD, " +
-                        "is_Kod AS KODU,is_Ismi AS ADI " +
+                        "is_Kod AS KODU,is_Ismi AS ADI, " +
+                        "ISNULL((SELECT TOP 1 upl_kodu FROM URETIM_MALZEME_PLANLAMA WHERE upl_isemri = is_Kod AND upl_uretim_tuket = 1),'') AS STOKKODU, " +
+                        "ISNULL((SELECT sto_isim  FROM STOKLAR WHERE sto_kod = ISNULL((SELECT TOP 1 upl_kodu FROM URETIM_MALZEME_PLANLAMA WHERE upl_isemri = is_Kod AND upl_uretim_tuket = 1),'')),'') AS STOKADI " +
                         "FROM ISEMIRLERI WHERE is_EmriDurumu = 1 AND is_Kod LIKE 'F%'"
             },
             selection : "KODU",
             columns :
             [
                 {
-                    dataField: "BARKOD",
-                    width: 200
-                }, 
-                {
                     dataField: "KODU",
                     width: 200
                 }, 
                 {
                     dataField: "ADI",
+                    width: 200
+                },
+                {
+                    dataField: "STOKADI",
                     width: 500
-                }
+                },
+                {
+                    dataField: "STOKKODU",
+                    width: 500
+                },
+                {
+                    dataField: "BARKOD",
+                    width: 200
+                }, 
             ],
             onSelected : async function(pData)
             {
