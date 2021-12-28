8297e07e69049b4718056a9e44aa2ff751975d9e
volkanturantw <volkan0223@gmail.com>
2021-12-28 12:07:02 +0300

    ....

--- www/js/ctrl/kuppeli/StokTanitim.js
+++ www/js/ctrl/kuppeli/StokTanitim.js
@@ -30,12 +30,393 @@ function StokTanitim($scope,srv,$rootScope,$filter)
         }     
        
     }
+    $scope.BteAnagrup = 
+        {
+            title : "Ana Grup",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.Btealtgrup = 
+        {
+            title : "Alt Grup",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.Btealtgrup2 = 
+        {
+            title : "Alt Grup2",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.Btemodel = 
+        {
+            title : "model",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        } 
+        $scope.Btetedarikci = 
+        {
+            title : "Tedarikci",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.Bteraf = 
+        {
+            title : "Raf",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.Btemateryal = 
+        {
+            title : "Materyal",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.BteKaplama = 
+        {
+            title : "Kaplama",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.Btetasrengi = 
+        {
+            title : "Tas Rengi",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.Btezincirsayisi = 
+        {
+            title : "Zincir Sayisi",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.Btezincirengi = 
+        {
+            title : "Zincir Rengi",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.Btefigur = 
+        {
+            title : "Figür",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+        $scope.Btefigursekli = 
+        {
+            title : "Figür Sekli",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+        $scope.Btefigurrengi = 
+        {
+            title : "Figür Rengi",
+            
+            datasource : 
+            {
+                db : "{M}." + $scope.Firma,
+                query : " ",
+            },
+            selection : "",
+            columns :
+            [
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+                {
+                    dataField: "",
+                    width: 200
+                }, 
+            ],
+            onClick : function(pCallback)
+            {                                
+                pCallback(true)
+            }
+        }
+
+
+
+    
     $scope.Init = async function()
     {        
         $scope.Firma = localStorage.getItem('firm');
         $scope.Param = srv.GetParam(atob(localStorage.getItem('login')));
         $rootScope.PageName = "StokTanitim"
         $scope.BasimAdet,
+        $scope.Barkod,
+        
         
         
         InitObj();
